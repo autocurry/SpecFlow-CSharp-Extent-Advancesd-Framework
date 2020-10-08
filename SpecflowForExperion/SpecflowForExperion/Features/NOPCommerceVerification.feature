8cca24101d216ed88c0a7a46849bfe3aa2684ede
autocurry <sandeepsekhar555@gmail.com>
2020-10-08 12:08:06 +0530

    Initial Commit

---
+++ SpecflowForExperion/SpecflowForExperion/Features/NOPCommerceVerification.feature
@@ -0,0 +1,8 @@
+﻿Feature: NOPCommerceVerification
+
+@chrome
+Scenario: Verify the All time pending order total value
+	Given I have access to the NOPCommerceWebsite
+	When I navigate to the admin page
+	Then Verify the All time pending order value is 2468.80
+
---
+++ SpecflowForExperion/SpecflowForExperion/Steps/NOPCommerceVerificationSteps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecflowForExperion.Steps
+{
+    [Binding]
+    public sealed class NOPCommerceVerificationSteps
+    {
+        [Given(@"I have access to the NOPCommerceWebsite")]
+        public void GivenIHaveAccessToTheNOPCommerceWebsite()
+        {
+            
+        }
+
+        [When(@"I navigate to the (.*) page")]
+        public void WhenINavigateToTheAdminPage(string pageName)
+        {
+            
+        }
+
+        [Then(@"Verify the All time pending order value is (.*)")]
+        public void ThenVerifyTheAllTimePendingOrderValueIs(double alltimevalue)
+        {
+            
+        }
+
+    }
+}
